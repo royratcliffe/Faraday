0db36b475b8762fc323dca45d024a7e708c06400
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-11-08 18:07:39 +0000

    JSON tests

---
+++ FaradayTests/JSONTests.swift
@@ -0,0 +1,56 @@
+// FaradayTests JSONTests.swift
+//
+// Copyright © 2015, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import XCTest
+import Foundation
+
+class JSONTests: XCTestCase {
+
+  func testInvalidJSONObject() {
+    XCTAssertFalse(NSJSONSerialization.isValidJSONObject(false))
+    XCTAssertFalse(NSJSONSerialization.isValidJSONObject(0))
+    XCTAssertFalse(NSJSONSerialization.isValidJSONObject("string"))
+  }
+
+  func testValidJSONObject() {
+    XCTAssertTrue(NSJSONSerialization.isValidJSONObject([]))
+    XCTAssertTrue(NSJSONSerialization.isValidJSONObject([:]))
+  }
+
+  func testAllowFragments() {
+    XCTAssertNotNil(JSONTests.fragment("false"))
+    XCTAssertNotNil(JSONTests.fragment("0"))
+    XCTAssertNotNil(JSONTests.fragment("\"string\""))
+  }
+
+  /// Converts a string to a JSON object, even if the string represents a JSON
+  /// fragment; that is, not an object but a primitive.
+  static func fragment(string: String) -> AnyObject? {
+    guard let data = NSString(string: string).dataUsingEncoding(NSUTF8StringEncoding) else {
+      return nil
+    }
+    return try? NSJSONSerialization.JSONObjectWithData(data, options: [.AllowFragments])
+  }
+
+}
